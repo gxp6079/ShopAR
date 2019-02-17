741bc99146b183a8791a3578688e27a3c2eb1214
gihpuccini <gxp6079@g.rit.edu>
2019-02-17 00:39:28 -0500

    basic scanner functionality

---
+++ app/src/main/java/com/blade/ShopAR/SimpleScannerActivity.java
@@ -0,0 +1,8 @@
+package com.blade.ShopAR;
+
+import android.app.Activity;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class SimpleScannerActivity extends Activity implements ZXingScannerView.ResultHandler {
+}
