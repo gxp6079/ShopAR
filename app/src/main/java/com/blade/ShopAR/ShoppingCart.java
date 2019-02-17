a1e4c2a2f58cd382b4955761de378fe41e31617e
gihpuccini <gxp6079@g.rit.edu>
2019-02-17 05:17:06 -0500

    Basic Shopping cart

--- app/src/main/java/com/blade/ShopAR/ShoppingCart.java
+++ app/src/main/java/com/blade/ShopAR/ShoppingCart.java
@@ -1,4 +1,19 @@
 package com.blade.ShopAR;
 
 public class ShoppingCart {
+
+    private int total;
+
+    public ShoppingCart(){
+        this.total = 0;
+    }
+
+    public int getTotal(){
+        return this.total;
+    }
+
+    public void addToCart(int price){
+        this.total += price;
+    }
+
 }
